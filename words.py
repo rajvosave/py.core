be8b1fcc6086ebc52f261d20a1594233bd697eab
rajvosave <vdunkic@gmail.com>
2020-04-08 09:28:21 +0200

    dorada

--- words.py
+++ words.py
@@ -1,16 +1,28 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    story = urlopen('http://sixty-north.com/c/t.txt')
+
+def fetch_words(url):
+    #http://sixty-north.com/c/t.txt
+    story = urlopen(url)
     story_words = []
     for line in story:
         line_words = line.decode('utf8').split()
         for word in line_words:
             story_words.append(word)
     story.close()
+    return story_words
 
-    for word in story_words:
-        print(word)
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
+
 
 if __name__ == '__main__':
-    fetch_words()
+    main(sys.argv[1])
