46f840e83a9233fe84a2252945618e0ceaa01dc1
vedran dunkić <vdunkic@gmail.com>
2020-04-14 11:12:55 +0000

    dodan avion

--- airtravel.py
+++ airtravel.py
@@ -17,3 +17,24 @@ class Flight:
 
     def number(self):
         return self._number
+
+    def airline(self):
+        return self._number[:2]
+
+
+class Aircraft:
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registraton = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+
+    def registration(self):
+        return self._registraton
+
+    def model(self):
+        return self._model
+
+    def seating_plan(self):
+        return (range(1, self._num_rows + 1),
+                "ABCDEFGJK"[:self._num_seats_per_row])
